072047cb10488daf510418ced69a158372d332ef
nitika-goel <nitika.goel@somish.com>
2017-11-04 18:40:53 +0530

    Add proposal status and category

--- contracts/governanceData.sol
+++ contracts/governanceData.sol
@@ -16,7 +16,7 @@
 
 pragma solidity ^0.4.8;
 
-contract governanceData {
+contract testData {
 
     struct proposal{
         address owner;
@@ -25,6 +25,14 @@ contract governanceData {
         uint date_add;
         uint date_upd;
         uint versionNum;
+        uint status;  
+        uint category;
+    }
+    struct proposalCategory{
+        address categorizedBy;
+        uint[] paramInt;
+        bytes32[] paramBytes32;
+        address[] paramAddress;
     }
     struct proposalVersionData{
         uint versionNum;
@@ -32,54 +40,166 @@ contract governanceData {
         string longDesc;
         uint date_add;
     }
+    struct Status{
+        uint movedTo;
+        uint date;
+    }
+    struct category
+    {
+        string categoryName;
+        uint8 memberVoteRequired;
+        uint8 majorityVote;
+        string functionName;
+        address contractAt;
+        uint8 paramInt;
+        uint8 paramBytes32;
+        uint8 paramAddress;      
+    }
+
+    mapping(uint=>proposalCategory) allProposalCategory;
+    category[] public allCategory;
+    string[] public status;
     proposal[] allProposal;
     mapping(uint=>proposalVersionData[]) proposalVersions;
+    mapping (uint=>Status[]) proposalStatus;
+    mapping (address=>uint8) public advisoryBoardMembers;
 
+    /// @dev Proposal.. and version..
     /// @dev Creates a new proposal 
     function addNewProposal(string _shortDesc,string _longDesc) public
     {
-        allProposal.push(proposal(msg.sender,_shortDesc,_longDesc,now,now,0));
+        allProposal.push(proposal(msg.sender,_shortDesc,_longDesc,now,now,0,0,0));
+
     }
 
     /// @dev Fetch details of proposal by giving proposal Id
-    function getProposalDetailsById(uint _id) public constant returns (address owner,string shortDesc,string longDesc,uint date_add,uint date_upd,uint versionNum)
+    function getProposalDetailsById(uint _id) public constant returns (address owner,string shortDesc,string longDesc,uint date_add,uint date_upd,uint versionNum,uint status)
     {
-        return (allProposal[_id].owner,allProposal[_id].shortDesc,allProposal[_id].longDesc,allProposal[_id].date_add,allProposal[_id].date_upd,allProposal[_id].versionNum);
+        return (allProposal[_id].owner,allProposal[_id].shortDesc,allProposal[_id].longDesc,allProposal[_id].date_add,allProposal[_id].date_upd,allProposal[_id].versionNum,allProposal[_id].status);
     }
       
     /// @dev Edits a proposal and Only owner of a proposal can edit it.
     function editProposal(uint _id , string _shortDesc, string _longDesc) public
     {
-        if(msg.sender == allProposal[_id].owner)
+        require(msg.sender == allProposal[_id].owner);
         {
             storeProposalVersion(_id);
             updateProposal(_id,_shortDesc,_longDesc);
         }
-        else
-            throw;
     }
 
     /// @dev Stores the information of a given version number of a given proposal. Maintains the record of all the versions of a proposal.
-    function storeProposalVersion(uint _id) public 
+    function storeProposalVersion(uint _id) private 
     {
-        uint versionNum = allProposal[_id].versionNum;
-        proposalVersions[_id].push(proposalVersionData(versionNum,allProposal[_id].shortDesc,allProposal[_id].longDesc,allProposal[_id].date_upd));            
+       
+        proposalVersions[_id].push(proposalVersionData(allProposal[_id].versionNum,allProposal[_id].shortDesc,allProposal[_id].longDesc,allProposal[_id].date_add));            
     }
 
     /// @dev Edits the details of an existing proposal and creates new version.
-    function updateProposal(uint _id,string _shortDesc,string _longDesc) public
+    function updateProposal(uint _id,string _shortDesc,string _longDesc) private
     {
         allProposal[_id].shortDesc = _shortDesc;
         allProposal[_id].longDesc = _longDesc;
         allProposal[_id].date_upd = now;
+        allProposal[_id].date_add = now;
         allProposal[_id].versionNum += 1;
     }
 
     /// @dev Gets version details of a given proposal id.
-    function getProposalDetailsByIdAndVersion(uint _proposalId,uint _versionNum) public constant returns( uint versionNum,string shortDesc,string longDesc,uint date_add)
+    function getProposalDetailsByIdAndVersion(uint _id,uint _versionNum) public constant returns( uint versionNum,string shortDesc,string longDesc,uint date_add)
     {
-       return (proposalVersions[_proposalId][_versionNum].versionNum,proposalVersions[_proposalId][_versionNum].shortDesc,proposalVersions[_proposalId][_versionNum].longDesc,proposalVersions[_proposalId][_versionNum].date_add);
+       return (proposalVersions[_id][_versionNum].versionNum,proposalVersions[_id][_versionNum].shortDesc,proposalVersions[_id][_versionNum].longDesc,proposalVersions[_id][_versionNum].date_add);
+    }
+    /// @dev Status..
+    /// @dev Changes the status of a given proposal.
+    function changeProposalStatus(uint _id,uint _status) internal
+    {
+        
+        pushInProposalStatus(_id,_status);
+        updateProposalStatus(_id,_status);
+        
+
     }
 
-}
+    /// @dev Adds status names in array - Not generic right now
+    function addStatus() public
+    {   
+        status.push("Draft for discussion, multiple versions.");
+        status.push("Pending-Advisory Board Vote");
+        status.push("Pending-Advisory Board Vote Accepted, pending Member Vote");
+        status.push("Final-Advisory Board Vote Declined");
+        status.push("Final-Advisory Board Vote Accepted, Member Vote not required");
+        status.push("Final-Advisory Board Vote Accepted, Member Vote Accepted");
+        status.push("Final-Advisory Board Vote Accepted, Member Vote Declined");
+        status.push("Final-Advisory Board Vote Accepted, Member Vote Quorum not Achieved");
+        status.push("Proposal Accepted, Insufficient Funds");
+    }
+   
+
+    /// @dev Updates  status of an existing proposal.
+    function updateProposalStatus(uint _id ,uint _status) public
+    {
+        allProposal[_id].status = _status;
+         allProposal[_id].date_upd =now;
+    }
+
+    /// @dev Stores the status information of a given proposal.
+    function pushInProposalStatus(uint _id , uint _status) public
+    {
+        proposalStatus[_id].push(Status(_status,now));
+    }
+    /// @dev category..
+    /// @dev Adds a new category.
+    function addNewCategory(string _categoryName,uint8 _memberVoteRequired,uint8 _majorityVote,string _functionName,address _contractAt,uint8 _paramInt,uint8 _paramBytes32,uint8 _paramAddress) public
+    {
+        allCategory.push(category(_categoryName,_memberVoteRequired,_majorityVote,_functionName,_contractAt,_paramInt,_paramBytes32,_paramAddress));
+    }
+
+    /// @dev Gets category details by category id.
+    function getCategoryDetails(uint _categoryId) public constant returns (string categoryName,uint64 memberVoteRequired,uint16 majorityVote,string functionName,address contractAt,uint16 paramInt,uint16 paramBytes32,uint16 paramAddress)
+    {    
+        categoryName = allCategory[_categoryId].categoryName;
+        memberVoteRequired = allCategory[_categoryId].memberVoteRequired;
+        majorityVote = allCategory[_categoryId].majorityVote;
+        functionName = allCategory[_categoryId].functionName;
+        contractAt = allCategory[_categoryId].contractAt;
+        paramInt = allCategory[_categoryId].paramInt;
+        paramBytes32 = allCategory[_categoryId].paramBytes32;
+        paramAddress = allCategory[_categoryId].paramAddress;
+    } 
+
+    function updateCategory(uint _categoryId,string _categoryName,uint64 _memberVoteRequired,uint16 _majorityVote,string _functionName,address _contractAt,uint8 _paramInt,uint8 _paramBytes32,uint8 _paramAddress)
+    {
+        allCategory[_categoryId].categoryName = _categoryName;
+        allCategory[_categoryId].functionName = _functionName;
+        allCategory[_categoryId].contractAt = _contractAt;
+        allCategory[_categoryId].paramInt = _paramInt;
+        allCategory[_categoryId].paramBytes32 = _paramBytes32; 
+        allCategory[_categoryId].paramAddress = _paramAddress; 
+    }
+
+    function categorizeProposal(uint _id , uint _categoryId,uint[] _paramInt,bytes32[] _paramBytes32,address[] _paramAddress)
+    {
+         require(advisoryBoardMembers[msg.sender]==1);
+        
+                uint16 paramInt; uint16 paramBytes32; uint16 paramAddress;
+                (,,,,,paramInt,paramBytes32,paramAddress) = getCategoryDetails(_categoryId);
+                if(paramInt == _paramInt.length && paramBytes32 == _paramBytes32.length && paramAddress == _paramAddress.length)
+                {
+                    allProposal[_id].category = _categoryId;
+                    allProposalCategory[_id]=proposalCategory(msg.sender,_paramInt,_paramBytes32,_paramAddress);
+                }
+      
+    }
+
+
+    function isProposalCategorised(uint _id) public constant returns(uint check)
+    {   
+        check=allProposal[_id].category;
+    }
+
+ 
+
+}  
+
 
