70292a5bace7a569e25fcbbbe99a664877459605
nitika-goel <nitika.goel@somish.com>
2017-11-07 19:52:55 +0530

    Updated voting closure

--- contracts/governanceData.sol
+++ contracts/governanceData.sol
@@ -27,6 +27,7 @@ contract governanceData {
         uint versionNum;
         uint status;  
         uint category;
+        int finalVerdict;
     }
     struct proposalCategory{
         address categorizedBy;
@@ -74,7 +75,19 @@ contract governanceData {
         owner = msg.sender;
         addCategory();
         addStatus();
+        //new
+        proposalVoteClosingTime = 60;
+        pendingProposalStart=0;
+        quorumPercentage=25;
+        //new
     }
+
+    //new
+    uint public proposalVoteClosingTime;
+    uint public quorumPercentage;
+    uint public pendingProposalStart;
+    uint public memberCounter;
+    //new
 
     mapping(uint=>proposalCategory) allProposalCategory;
     category[] public allCategory;
@@ -93,6 +106,179 @@ contract governanceData {
     proposalVote[] allVotes;
     uint public totalVotes;
 
+    //new
+    /// @dev Increases the count of NXM Members by 1 (called whenever a new Member is added).
+    function incMemberCounter() internal
+    {
+        memberCounter++;
+    }
+
+    /// @dev Decreases the count of NXM Members by 1 (called when a member is removed i.e. NXM tokens of a member is 0).
+    function decMemberCounter() internal
+    {
+        memberCounter--;
+    }
+
+    /// @dev Gets the Quorum Percentage.
+    function getQuorumPerc() public constant returns(uint percentage) 
+    {
+        percentage = quorumPercentage;
+    }
+
+    /// @dev Changes the Quorum Percentage.
+    function changeQuorumPercentage(uint _percentage) public  
+    {
+        quorumPercentage = _percentage;
+    }
+
+    /// @dev Changes the status of a given proposal to open it for voting. // wil get called when we submit the proposal
+    function openProposalForVoting(uint _proposalId) public
+    {
+        require(allProposal[_proposalId].owner == msg.sender || allProposal[_proposalId].status ==0);
+        allProposal[_proposalId].status = 1;
+        // closeProposalOraclise(id,gd1.getClosingTime());
+    }
+
+    /// @dev Changes the time(in seconds) after which proposal voting is closed.
+    function changeProposalVoteClosingTime(uint _closingTime) public
+    {
+        proposalVoteClosingTime = _closingTime;   
+    }
+
+    // function changeClosingTime(uint _closingTime) public
+    // {
+    //     uint proposalLength = allProposal.length;
+    //     uint pendingProposalStart = getPendingProposalStart();
+    //     for(uint i=pendingProposalStart; i<len ;i++)
+    //     {
+    //         require(allProposal[i].date_upd + _closingTime <= now);
+    //         closeProposalVote(i);
+    //         // }
+    //         // else
+    //         // {
+    //         //     uint64 timeleft = uint64(gd1.getProposalDateUpd(i)+_time -now);
+    //         //     p1=pool(poolAd);
+    //         //     p1.closeProposalOraclise(i,timeleft);
+    //         // }
+    //     }
+    // }   
+
+    function getPendingProposalStart() returns(uint pendingPS) 
+    {
+        pendingPS = pendingProposalStart;
+    }
+    
+    /// @dev Checks if voting time of a given proposal should be closed or not. 
+    function checkProposalVoteClosing(uint _proposalId) constant returns(uint8 closeValue)
+    {
+        require((allProposal[_proposalId].date_upd + proposalVoteClosingTime <= now && allProposal[_proposalId].status == 1)|| allProposal[_proposalId].status == 2);
+        closeValue=1;
+    }
+
+    /// @dev proposal should gets closed.
+    function closeProposalVote(uint _proposalId)
+    {
+        if(checkProposalVoteClosing(_proposalId)==1) /// either status == 1 or status == 2 thats why Closing ==1
+        {
+            uint acceptABvote;
+            uint denyABvote;
+            uint acceptMemberVote;
+            uint denyMemberVote;
+            uint8 majorityVote;
+            uint8 memberVoteRequired;
+            uint category = allProposal[_proposalId].category;
+            uint totalMember = memberCounter; 
+
+            (,memberVoteRequired,majorityVote,,,,,) = getCategoryDetails(category);
+            if(allProposal[_proposalId].status==1)  // // pending advisory board vote
+            {
+                (acceptABvote,denyABvote,,) = getProposalVoteCount(_proposalId);
+                
+                if(acceptABvote*100/(acceptABvote+denyABvote)>=majorityVote) /// if proposal accepted% >=majority % (by Advisory board)
+                {    
+                    if(memberVoteRequired==1) /// Member vote required 
+                    {
+                        changeProposalStatus(_proposalId,2);
+                        // p1.closeProposalOraclise(id,gd1.getClosingTime());
+                    }
+                    else /// Member vote not required
+                    {
+                        changeProposalStatus(_proposalId,4);
+                        actionAfterProposalPass(_proposalId , category);
+                    }
+                } // when AB votes are not enough . /// if proposal is denied
+                else
+                {
+                    changeProposalStatus(_proposalId,3);
+                    allProposal[_proposalId].finalVerdict = -1;
+                    actionAfterProposalPass(_proposalId , category); // TEMPORARY 
+                }
+            }
+            else if(allProposal[_proposalId].status==2) /// pending member vote
+            {
+                (,,acceptMemberVote,denyMemberVote) = getProposalVoteCount(_proposalId);
+                /// when Member Vote Quorum not Achieved
+                if((acceptMemberVote+denyMemberVote)*100/totalMember < getQuorumPerc()) 
+                {
+                    changeProposalStatus(_proposalId,7);
+                    allProposal[_proposalId].finalVerdict = 1;
+                    actionAfterProposalPass(_proposalId , category);
+                }
+                /// if proposal accepted% >=majority % (by Members)
+                else if(acceptMemberVote*100/(acceptMemberVote+denyMemberVote)>=majorityVote)
+                {
+                    changeProposalStatus(_proposalId,5);
+                    allProposal[_proposalId].finalVerdict = 1;
+                    actionAfterProposalPass(_proposalId , category);
+                }
+                /// if proposal is denied
+                else
+                {
+                    changeProposalStatus(_proposalId,6);
+                    allProposal[_proposalId].finalVerdict = -1;    
+                }
+            }
+        } 
+        uint pendingPS = pendingProposalStart;
+        uint proposalLength = allProposal.length;
+        for(uint j=pendingPS; j<proposalLength; j++)
+        {
+            if(allProposal[j].status > 2)
+                pendingPS += 1;
+            else
+                break;
+        }
+        if(j!=pendingPS)
+        {
+            changePendingProposalStart(j);
+        }
+        
+    }
+
+    /// @dev Change pending proposal start variable
+    function changePendingProposalStart(uint _pendingPS) internal
+    {
+        pendingProposalStart = _pendingPS;
+    }
+
+    function actionAfterProposalPass(uint256 _proposalId,uint _categoryId) public
+    {
+        address contractAt = allCategory[_categoryId].contractAt;
+        // string  functionName = allCategory[_categoryId].functionName;
+        // string functionName = "updateCategory_memberVote(uint)";
+        contractAt.call(bytes4(sha3(allCategory[_categoryId].functionName)),_proposalId);
+    }
+
+    function updateCategoryMVR(uint _categoryId) 
+    {
+        allCategory[_categoryId].memberVoteRequired = 1;
+    }
+    //new
+
+
+
+
+    /// @dev Check if the member who wants to change in contracts, is owner.
     function isOwner(address _memberAddress) constant returns(uint checkOwner)
     {
         checkOwner=0;
@@ -100,6 +286,7 @@ contract governanceData {
             checkOwner=1;
     }
 
+    /// @dev Change current owner
     function changeOwner(address _memberAddress) public
     {
         if(owner == msg.sender)
@@ -121,15 +308,6 @@ contract governanceData {
     function increaseTotalVotes() internal
     {
         totalVotes++;
-    }
-
-
-    /// @dev Changes the status of a given proposal to open it for voting.
-    function changeProposalVotingStatus(uint _id)
-    {
-        if(allProposal[_id].owner != msg.sender || allProposal[_id].status !=0) throw;
-        changeProposalStatus(_id,1);
-        // closeProposalOraclise(id,gd1.getClosingTime());
     }
 
     /// @dev Registers an Advisroy Board Member's vote for Proposal. _id is proposal id..
@@ -205,7 +383,7 @@ contract governanceData {
     /// @dev Creates a new proposal 
     function addNewProposal(string _shortDesc,string _longDesc) public
     {
-        allProposal.push(proposal(msg.sender,_shortDesc,_longDesc,now,now,0,0,0));
+        allProposal.push(proposal(msg.sender,_shortDesc,_longDesc,now,now,0,0,0,0));
     }
 
     /// @dev Fetch details of proposal by giving proposal Id
@@ -222,7 +400,6 @@ contract governanceData {
     /// @dev Edits a proposal and Only owner of a proposal can edit it.
     function editProposal(uint _id , string _shortDesc, string _longDesc) public
     {
-        uint8[] paramInt; bytes32 paramBytes32; address[] paramAddress;
         require(msg.sender == allProposal[_id].owner);
         {
             storeProposalVersion(_id);
@@ -233,8 +410,6 @@ contract governanceData {
             allProposalCategory[_id].paramAddress.push(0);
         }
     }
-
-
 
     /// @dev Stores the information of a given version number of a given proposal. Maintains the record of all the versions of a proposal.
     function storeProposalVersion(uint _id) internal 
@@ -283,7 +458,7 @@ contract governanceData {
     function addCategory() internal
     {   
         allCategory.push(category("Uncategorised",0,0,"",0,0,0,0));
-        allCategory.push(category("Filter member proposals as necessary(which are put to a member vote)",0,60,"",0,1,2,1));
+        allCategory.push(category("Filter member proposals as necessary(which are put to a member vote)",0,60,"",0,0,0,0));
         allCategory.push(category("Implement run-off and close new business",1,80,"",0,0,0,0));
         allCategory.push(category("Burn fraudulent claim assessor tokens",0,80,"",0,0,0,0));
         allCategory.push(category("Pause Claim Assessors ability to assess claims for 3 days.Can only be done once a month",0,60,"",0,0,0,0));
@@ -296,12 +471,6 @@ contract governanceData {
         allCategory.push(category("Any change to authorities",1,80,"",0,0,0,0));
         allCategory.push(category("Any other item specifically described",1,80,"",0,0,0,0));
     }
-
-    function updateCategory_memberVote(uint _categoryId,uint8 _memberVoteRequired) public
-    {
-        allCategory[_categoryId].memberVoteRequired = _memberVoteRequired;
-    }
-    
 
     /// @dev Updates  status of an existing proposal.
     function updateProposalStatus(uint _id ,uint _status) internal
@@ -373,5 +542,24 @@ contract governanceData {
     }
 
 }  
+
+contract governance 
+{
+   address governanceDataAddress;
+    governanceData gd1;
+    function changeGovernanceDataAddress(address _contractAddress) public
+    {
+        governanceDataAddress = _contractAddress;
+        gd1=governanceData(governanceDataAddress);
+    }
+    
+    function updateCategory_memberVote(uint256 _proposalId) 
+    {
+        gd1=governanceData(governanceDataAddress);
+        uint _categoryId = gd1.getProposalCategory(_proposalId);
+        gd1.updateCategoryMVR(_categoryId);
+    }
+    
+}
 
 
