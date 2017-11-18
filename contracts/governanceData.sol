f97c7b277b5d6d0a83e19130b470e537d0e299ed
tech-somish <contact@somish.com>
2017-11-18 16:42:44 +0545

    added zeppelin-contract's functions

--- contracts/governanceData.sol
+++ contracts/governanceData.sol
@@ -80,6 +80,8 @@ contract governanceData{
     {
         uint[] ABVoteCount;
         uint[] MemberVoteCount;
+        uint totalTokenAB;
+        uint totalTokenMember;
     }
     
     mapping (uint => proposalVoteCount) allProposalVoteCount;
@@ -107,6 +109,27 @@ contract governanceData{
     mapping (address=>Status[])  memberAsABmember;
     proposalVote[] allVotes;
     uint public totalVotes;
+    address basicTokenAddress;
+    BasicToken basicToken;
+
+    /// @dev Change basic token contract's address
+    function changeBasicTokenAddress(address _contractAddress) public
+    {
+        basicTokenAddress = _contractAddress;
+        basicToken=BasicToken(basicTokenAddress);
+    }
+
+    function getBalanceOfMember(address _memberAddress) public constant returns (uint totalToken)
+    {
+        basicToken=BasicToken(basicTokenAddress);
+        totalToken = basicToken.balanceOf(_memberAddress);
+        if(advisoryBoardMembers[_memberAddress]==1)
+            allProposalVoteCount.totalTokenAB +=1;
+        else
+            allProposalVoteCount.totalTokenMember+=1;
+    }
+
+    
 
     /// @dev Get the vote count(voting done by AB) for options of proposal when giving Proposal id and Option index.
     function getproposalABVoteCount(uint _proposalId,uint _index) constant returns(uint result)
