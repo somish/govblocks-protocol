5afde674d857450b122ae5f7bf413d71c9a3360c
tech-somish <contact@somish.com>
2017-12-18 13:24:35 +0530

    Using safemath library - Rank Based Voting

--- contracts/RankBasedVoting.sol
+++ contracts/RankBasedVoting.sol
@@ -331,12 +331,12 @@ contract RankBasedVoting is VotingType
             uint voteid = GD.getVoteIdByProposalId(_proposalId,i);
            if(allVotes[voteid].verdictChosen[0] == finalVerdict)
             {
-                voteValueFavour = voteValueFavour + allVotes[voteid].voteValue;
+                voteValueFavour = SafeMath.add(voteValueFavour,allVotes[voteid].voteValue);
             }
             else
             {
-                voterStake = voterStake + allVotes[voteid].voteStakeGNT*(1/GD.globalRiskFactor());
-                returnTokens = allVotes[voteid].voteStakeGNT*(1-(1/GD.globalRiskFactor()));
+                voterStake = SafeMath.add(voterStake,SafeMath.mul(allVotes[voteid].voteStakeGNT,(SafeMath.div(SafeMath.mul(1,100),GD.globalRiskFactor()))));
+                returnTokens = SafeMath.mul(allVotes[voteid].voteStakeGNT,(SafeMath.sub(1,(SafeMath.div(SafeMath.mul(1,100),GD.globalRiskFactor())))));
                 GD.transferBackGNTtoken(allVotes[voteid].voter,returnTokens);
             }
         }
@@ -344,16 +344,16 @@ contract RankBasedVoting is VotingType
         for(i=0; i<GD.getVerdictAddedAddressLength(_proposalId); i++)
         {
             if(i!= finalVerdict)         
-                 wrongOptionStake = wrongOptionStake + GD.getVerdictStakeByProposalId(_proposalId,i);
+                wrongOptionStake = SafeMath.add(wrongOptionStake,GD.getVerdictStakeByProposalId(_proposalId,i));
         }
 
-        totalVoteValue = GD.getVerdictValueByProposalId(_proposalId,finalVerdict) + voteValueFavour; 
-        totalTokenToDistribute = wrongOptionStake + voterStake;
+        totalVoteValue = SafeMath.add(GD.getVerdictValueByProposalId(_proposalId,finalVerdict),voteValueFavour);
+        totalTokenToDistribute = SafeMath.add(wrongOptionStake,voterStake);
 
-        if(finalVerdict>0)
-            totalVoteValue = totalVoteValue + proposalValue;
+       if(finalVerdict>0)
+            totalVoteValue = SafeMath.add(totalVoteValue,proposalValue);
         else
-            totalTokenToDistribute = totalTokenToDistribute + proposalStake;
+            totalTokenToDistribute = SafeMath.add(totalTokenToDistribute,proposalStake);
 
         distributeReward(_proposalId,totalTokenToDistribute,totalVoteValue,proposalStake);
     }
@@ -367,25 +367,16 @@ contract RankBasedVoting is VotingType
  
         if(finalVerdict > 0)
         {
-            reward = (_proposalStake*_totalTokenToDistribute)/_totalVoteValue;
-            transferToken = _proposalStake + reward;
+            reward = SafeMath.div(SafeMath.mul(_proposalStake,_totalTokenToDistribute),_totalVoteValue);
+            transferToken = SafeMath.add(_proposalStake,reward);
             GD.transferBackGNTtoken(proposalOwner,transferToken);
 
             address verdictOwner = GD.getVerdictAddressByProposalId(_proposalId,finalVerdict);
-            uint verdictStake =GD.getVerdictStakeByProposalId(_proposalId,finalVerdict);
-            reward = (verdictStake*_totalTokenToDistribute)/_totalVoteValue;
-            transferToken = verdictStake + reward;
+            uint verdictStake = GD.getVerdictStakeByProposalId(_proposalId,finalVerdict);
+            reward = SafeMath.div(SafeMath.mul(verdictStake,_totalTokenToDistribute),_totalVoteValue);
+            transferToken = SafeMath.add(verdictStake,reward);
             GD.transferBackGNTtoken(verdictOwner,transferToken);
         }
-
-        // for(uint i=0; i<GD.getTotalVoteLengthAgainstProposal(_proposalId); i++)
-        // {
-        //     uint voteid = GD.getVoteIdByProposalId(_proposalId,i);
-        //     require(allVotes[voteid].verdictChosen[0] == finalVerdict);
-        //         reward = (allVotes[voteid].voteValue*_totalTokenToDistribute)/_totalVoteValue;
-        //         transferToken = allVotes[voteid].voteStakeGNT + reward;
-        //         GD.transferBackGNTtoken(allVotes[voteid].voter,transferToken);
-        // }
 
         for(uint i=0; i<GD.getTotalVoteLengthAgainstProposal(_proposalId); i++)
         {
@@ -393,16 +384,15 @@ contract RankBasedVoting is VotingType
             for(uint j=0; j<allVotes[voteid].verdictChosen.length; j++)
             {
                 require(allVotes[voteid].verdictChosen[j] == finalVerdict);
-                    uint optionValue = distributeReward1(voteid,_proposalId,j);
-                    reward = ((allVotes[voteid].voteValue*optionValue)*_totalTokenToDistribute)/_totalVoteValue;
-                    transferToken = allVotes[voteid].voteStakeGNT + reward;
+                    uint optionValue = getOptionValue(voteid,_proposalId,j);
+                    reward = SafeMath.div(SafeMath.mul(allVotes[voteid].voteValue,_totalTokenToDistribute),_totalVoteValue);
+                    transferToken = SafeMath.add(allVotes[voteid].voteStakeGNT,reward);
                     GD.transferBackGNTtoken(allVotes[voteid].voter,transferToken);
-            }
-               
+            }        
         }
     }
 
-    function distributeReward1(uint voteid,uint _proposalId,uint _optionIndex) returns (uint optionValue)
+    function getOptionValue(uint voteid,uint _proposalId,uint _optionIndex) returns (uint optionValue)
     {
         uint[] _verdictChosen = allVotes[voteid].verdictChosen;
         uint _verdictOptions; 
