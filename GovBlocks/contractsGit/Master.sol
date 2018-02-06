15c91bba6ef7cecd86944ef7c03ea6a17f06e679
Shiksha <shiksha@dev.somish.com>
2018-02-06 21:57:32 +0530

    Rectified errors

---
+++ GovBlocks/contractsGit/Master.sol
@@ -0,0 +1,309 @@
+/* Copyright (C) 2017 GovBlocks.io
+
+  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ */
+    
+pragma solidity ^0.4.8;
+
+import "./GovernanceData.sol";
+import "./SimpleVoting.sol";
+import "./RankBasedVoting.sol";
+import "./FeatureWeighted.sol";
+import "./MemberRoles.sol";
+import "./ProposalCategory.sol";
+import "./StandardVotingType.sol";
+import "./Governance.sol";
+import "./Pool.sol";
+import "./GBTController.sol";
+import "./GBTStandardToken.sol";
+
+
+contract Master is Ownable {
+
+    struct contractDetails{
+        bytes32 name;
+        address contractAddress;
+    }
+
+    struct changeVersion{
+        uint date_implement;
+        uint versionNo;
+    }
+
+    uint  public versionLength;
+    changeVersion[]  contractChangeDate;
+    mapping(uint=>contractDetails[]) public allContractVersions;
+    mapping(address=>uint) public contracts_active;
+    
+    address governanceDataAddress;
+    address memberRolesAddress;
+    address proposalCategoryAddress;
+    address simpleVotingAddress;
+    address rankBasedVotingAddress;
+    address featureWeightedAddress;
+    address masterAddress;
+    address standardVotingTypeAddress;
+    address GBTOwner;
+    address governanceAddress;
+    address poolAddress;
+    address GBTCAddress;
+    address GBTSAddress;
+    GBTController GBTC;
+    GBTStandardToken GBTS;
+    Pool P1;
+    Governance G1;
+    GovernanceData GD;
+    MemberRoles MR;
+    ProposalCategory PC;
+    SimpleVoting SV;
+    RankBasedVoting RB;
+    FeatureWeighted FW;
+    StandardVotingType SVT;
+
+    /// @dev Constructor
+    function Master()
+    {
+        masterAddress=address(this);
+        contracts_active[masterAddress]=0;
+        contracts_active[address(this)]=1;
+        versionLength =0;
+    }
+   
+    modifier onlyGBTOwner
+    {
+      require(msg.sender == owner || msg.sender == GBTOwner);
+      _;
+    }
+
+    modifier onlyInternal 
+    {
+        require(contracts_active[msg.sender] == 1 || owner==msg.sender);
+        _; 
+    }
+
+    function GovBlocksOwner()
+    {
+        require(GBTOwner == 0x00);
+        GBTOwner = msg.sender;
+    }
+
+    function isInternal(address _contractAdd) constant returns(uint check)
+    {
+        check=0;
+        if(contracts_active[msg.sender] == 1 || owner==msg.sender)
+            check=1;
+    }
+
+    function isOwner(address _ownerAddress) constant returns(uint check)
+    {
+        check=0;
+        if(owner == _ownerAddress)
+            check=1;
+    }
+
+    /// @dev Creates a new version of contract addresses.
+    function addNewVersion(address[] _contractAddresses) onlyOwner
+    {
+        uint versionNo = versionLength;
+        setVersionLength(versionNo+1);
+        
+        addContractDetails(versionNo,"Master",masterAddress);
+        addContractDetails(versionNo,"GovernanceData",_contractAddresses[0]);
+        addContractDetails(versionNo,"MemberRoles",_contractAddresses[1]);
+        addContractDetails(versionNo,"ProposalCategory",_contractAddresses[2]); 
+        addContractDetails(versionNo,"SimpleVoting",_contractAddresses[3]);
+        addContractDetails(versionNo,"RankBasedVoting",_contractAddresses[4]); 
+        addContractDetails(versionNo,"FeatureWeighted",_contractAddresses[5]); 
+        addContractDetails(versionNo,"StandardVotingType",_contractAddresses[6]); 
+        addContractDetails(versionNo,"Governance",_contractAddresses[7]); 
+        addContractDetails(versionNo,"Pool",_contractAddresses[8]); 
+        addContractDetails(versionNo,"GBTController",_contractAddresses[9]); 
+        addContractDetails(versionNo,"GBTStandardToken",_contractAddresses[10]); 
+    }
+
+    /// @dev Adds Contract's name  and its ethereum address in a given version.
+    function addContractDetails(uint _versionNo,bytes32 _contractName,address _contractAddresse) internal
+    {
+        allContractVersions[_versionNo].push(contractDetails(_contractName,_contractAddresse));        
+    }
+
+    /// @dev Changes all reference contract addresses in master 
+    function changeAddressInMaster(uint _version) onlyInternal
+    {
+        changeAllAddress(_version);
+        governanceDataAddress = allContractVersions[_version][1].contractAddress;
+        memberRolesAddress = allContractVersions[_version][2].contractAddress;
+        proposalCategoryAddress = allContractVersions[_version][3].contractAddress;
+        simpleVotingAddress = allContractVersions[_version][4].contractAddress;
+        rankBasedVotingAddress = allContractVersions[_version][5].contractAddress;
+        featureWeightedAddress = allContractVersions[_version][6].contractAddress;
+        standardVotingTypeAddress = allContractVersions[_version][7].contractAddress;
+        governanceAddress = allContractVersions[_version][8].contractAddress;
+        poolAddress = allContractVersions[_version][9].contractAddress;
+        GBTCAddress = allContractVersions[_version][10].contractAddress;
+        GBTSAddress = allContractVersions[_version][11].contractAddress;
+    }
+
+    /// @dev Sets the older version contract address as inactive and the latest one as active.
+    function changeAllAddress(uint version) internal
+    {
+        addRemoveAddress(version,1);
+        addRemoveAddress(version,2);
+        addRemoveAddress(version,3);
+        addRemoveAddress(version,4);
+        addRemoveAddress(version,5);
+        addRemoveAddress(version,6);
+        addRemoveAddress(version,7);
+        addRemoveAddress(version,8);
+        addRemoveAddress(version,9);
+        addRemoveAddress(version,10);
+        addRemoveAddress(version,11);
+    }
+
+    /// @dev Deactivates address of a contract from last version.
+    function addRemoveAddress(uint _version,uint _index) internal
+    {
+        uint version_old=0;
+        if(_version>0)
+            version_old=_version-1;
+        contracts_active[allContractVersions[version_old][_index].contractAddress]=0;
+        contracts_active[allContractVersions[_version][_index].contractAddress]=1;
+    }
+
+    /// @dev Links all contracts to master.sol by passing address of Master contract to the functions of other contracts.
+    function changeMasterAddress(address _masterAddress) onlyOwner
+    {
+        GD=GovernanceData(governanceDataAddress);
+        GD.changeMasterAddress(_masterAddress);
+                             
+        SV=SimpleVoting(simpleVotingAddress);
+        SV.changeMasterAddress(_masterAddress);
+
+        RB=RankBasedVoting(rankBasedVotingAddress);
+        RB.changeMasterAddress(_masterAddress);
+
+        FW=FeatureWeighted(featureWeightedAddress);
+        FW.changeMasterAddress(_masterAddress);
+
+        SVT=StandardVotingType(standardVotingTypeAddress);
+        SVT.changeMasterAddress(_masterAddress);
+
+        G1=Governance(governanceAddress);
+        G1.changeMasterAddress(_masterAddress);
+
+        P1=Pool(poolAddress);
+        P1.changeMasterAddress(_masterAddress);
+
+        GBTC=GBTController(GBTCAddress);
+        GBTC.changeMasterAddress(_masterAddress);
+    }
+
+   /// @dev Link contracts to one another.
+   function changeOtherAddress() onlyInternal
+   {  
+        changeGBTAddress(GBTSAddress);
+        changeGBTControllerAddress(GBTCAddress);
+        
+        GD=GovernanceData(governanceDataAddress);
+        GD.changeAllContractsAddress(poolAddress);
+
+        SV=SimpleVoting(simpleVotingAddress);
+        SV.changeAllContractsAddress(standardVotingTypeAddress,governanceDataAddress,memberRolesAddress,proposalCategoryAddress);
+
+        RB=RankBasedVoting(rankBasedVotingAddress);
+        RB.changeAllContractsAddress(standardVotingTypeAddress,governanceDataAddress,memberRolesAddress,proposalCategoryAddress);
+
+        FW=FeatureWeighted(featureWeightedAddress);
+        FW.changeAllContractsAddress(standardVotingTypeAddress,governanceDataAddress,memberRolesAddress,proposalCategoryAddress);
+
+        SVT=StandardVotingType(standardVotingTypeAddress);
+        SVT.changeAllContractsAddress(governanceDataAddress,memberRolesAddress,proposalCategoryAddress);
+        SVT.changeOtherContractAddress(simpleVotingAddress,rankBasedVotingAddress,featureWeightedAddress);
+        SVT.changeOtherContractAddress1(governanceAddress,poolAddress);
+
+        G1=Governance(governanceAddress);
+        G1.changeAllContractsAddress(governanceDataAddress,memberRolesAddress,proposalCategoryAddress);
+        G1.changePoolAddress(poolAddress);
+        
+        PC=ProposalCategory(proposalCategoryAddress);
+        PC.changeAllContractsAddress(memberRolesAddress,governanceDataAddress);
+
+        P1=Pool(poolAddress);
+        P1.changeAllContractsAddress(governanceDataAddress,simpleVotingAddress);
+   }
+
+    /// @dev Change GBT token address all contracts
+    function changeGBTAddress(address _tokenAddress) onlyGBTOwner
+    {
+        GD=GovernanceData(governanceDataAddress);
+        GD.changeGBTtokenAddress(_tokenAddress);
+        
+        // SV=SimpleVoting(simpleVotingAddress);
+        // SV.changeGBTtokenAddress(_tokenAddress);
+
+        // RB=RankBasedVoting(rankBasedVotingAddress);
+        // RB.changeGBTtokenAddress(_tokenAddress);
+
+        // FW=FeatureWeighted(featureWeightedAddress);
+        // FW.changeGBTtokenAddress(_tokenAddress);
+
+        // SVT=StandardVotingType(standardVotingTypeAddress);
+        // SVT.changeGBTtokenAddress(_tokenAddress);
+
+        GBTC=GBTController(GBTCAddress);
+        GBTC.changeGBTtokenAddress(_tokenAddress);
+
+        P1=Pool(poolAddress);
+        P1.changeGBTtokenAddress(_tokenAddress);  
+    }
+
+    function changeGBTControllerAddress(address _controllerAddress)
+    {
+        G1=Governance(governanceAddress);
+        G1.changeGBTControllerAddress(_controllerAddress);
+
+        SV=SimpleVoting(simpleVotingAddress);
+        SV.changeGBTControllerAddress(_controllerAddress);
+
+        RB=RankBasedVoting(rankBasedVotingAddress);
+        RB.changeGBTControllerAddress(_controllerAddress);
+
+        FW=FeatureWeighted(featureWeightedAddress);
+        FW.changeGBTControllerAddress(_controllerAddress);
+        
+        P1=Pool(poolAddress);
+        P1.changeGBTControllerAddress(_controllerAddress);
+    }
+
+    /// @dev Switch to the recent version of contracts. (Last one)
+    function switchToRecentVersion() onlyInternal
+    {
+        uint version = versionLength-1;
+        addInContractChangeDate(now,version);
+        changeAddressInMaster(version);
+    }
+
+    /// @dev Stores the date when version of contracts get switched.
+    function addInContractChangeDate(uint _date , uint _versionNo) internal
+    {
+        contractChangeDate.push(changeVersion(_date,_versionNo));
+    }
+  
+    /// @dev Sets the length of version.
+    function setVersionLength(uint _length) internal
+    {
+        versionLength = _length;
+    }
+
+}
+
---
+++ GovBlocks/contractsGit/StandardVotingType.sol
@@ -0,0 +1,251 @@
+/* Copyright (C) 2017 GovBlocks.io
+
+  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ */
+
+/**
+ * @title votingType interface for All Types of voting.
+ */
+
+pragma solidity ^0.4.8;
+import "./SimpleVoting.sol";
+import "./RankBasedVoting.sol";
+import "./FeatureWeighted.sol";
+import "./GovernanceData.sol";
+import "./VotingType.sol";
+import "./Pool.sol";
+import "./Master.sol";
+import "./Governance.sol";
+
+
+contract StandardVotingType
+{
+    address SVAddress;
+    address RBAddress;
+    address FWAddress;
+    address GDAddress;
+    address MRAddress;
+    address PCAddress;
+    address VTAddress;
+    address G1Address;
+    address P1Address;
+    address public masterAddress;
+    Master MS;
+    Pool P1;
+    Governance G1;
+    MemberRoles MR;
+    ProposalCategory PC;
+    GovernanceData  GD;
+    BasicToken BT;
+    StandardVotingType SVT;
+    SimpleVoting SV;
+    RankBasedVoting RB;
+    FeatureWeighted FW;
+    VotingType VT;
+
+    modifier onlyInternal {
+        MS=Master(masterAddress);
+        require(MS.isInternal(msg.sender) == 1);
+        _; 
+    }
+
+/// @dev Change master's contract address
+    function changeMasterAddress(address _masterContractAddress)
+    {
+        if(masterAddress == 0x000)
+            masterAddress = _masterContractAddress;
+        else
+        {
+            MS=Master(masterAddress);
+            require(MS.isInternal(msg.sender) == 1);
+                masterAddress = _masterContractAddress;
+        }
+    }
+    
+    function changeOtherContractAddress(address _SVaddress,address _RBaddress,address _FWaddress) onlyInternal
+    {
+        SVAddress = _SVaddress;
+        RBAddress = _RBaddress;
+        FWAddress = _FWaddress;
+    }
+
+    function changeOtherContractAddress1(address _governanceContractAddress,address _poolContractAddress)
+    {
+        G1Address = _governanceContractAddress;
+        P1Address = _poolContractAddress;
+    }
+    
+    function changeAllContractsAddress(address _GDcontractAddress, address _MRcontractAddress, address _PCcontractAddress) onlyInternal
+    {
+        GDAddress = _GDcontractAddress;
+        MRAddress = _MRcontractAddress;
+        PCAddress = _PCcontractAddress;
+    }
+
+    // /// @dev Changes GBT contract Address. 
+    // function changeGBTtokenAddress(address _GBTcontractAddress) onlyInternal
+    // {
+    //     GBTAddress = _GBTcontractAddress;
+    // }
+    
+    function setOptionValue_givenByMemberSVT(address _memberAddress,uint _proposalId,uint _memberStake) internal returns (uint finalOptionValue)
+    {
+        GD=GovernanceData(GDAddress);
+        uint memberLevel = Math.max256(GD.getMemberReputation(_memberAddress),1);
+        uint tokensHeld = SafeMath.div((SafeMath.mul(SafeMath.mul(GD.getBalanceOfMember(_memberAddress),100),100)),GD.getTotalTokenInSupply());
+        uint maxValue= Math.max256(tokensHeld,GD.membershipScalingFactor());
+
+        finalOptionValue = SafeMath.mul(SafeMath.mul(GD.globalRiskFactor(),memberLevel),SafeMath.mul(_memberStake,maxValue));
+    }
+
+    function setVoteValue_givenByMember(address _memberAddress,uint _proposalId,uint _memberStake) onlyInternal returns (uint finalVoteValue)
+    {
+        GD=GovernanceData(GDAddress);
+
+        uint tokensHeld = SafeMath.div((SafeMath.mul(SafeMath.mul(GD.getBalanceOfMember(_memberAddress),100),100)),GD.getTotalTokenInSupply());
+        uint value= SafeMath.mul(Math.max256(_memberStake,GD.scalingWeight()),Math.max256(tokensHeld,GD.membershipScalingFactor()));
+        finalVoteValue = SafeMath.mul(GD.getMemberReputation(_memberAddress),value);
+    }  
+    
+    function addVerdictOptionSVT(uint _proposalId,address _memberAddress,uint _votingType,uint[] _paramInt,bytes32[] _paramBytes32,address[] _paramAddress,uint _GBTPayableTokenAmount,string _optionDescHash) onlyInternal
+    {
+        GD=GovernanceData(GDAddress);
+        MR=MemberRoles(MRAddress);
+        PC=ProposalCategory(PCAddress);
+
+        if(_votingType == 0)
+        {
+           SV = SimpleVoting(SVAddress);
+           VT = SV;
+        }
+        else if(_votingType == 1)
+        {
+            RB = RankBasedVoting(RBAddress);
+            VT = RB;
+        } 
+        else
+        {
+            FW = FeatureWeighted(FWAddress);
+            VT = FW;
+        }
+
+        uint currentVotingId;
+        (,,currentVotingId,,,) = GD.getProposalDetailsById2(_proposalId);
+
+        require(currentVotingId == 0 && GD.getProposalStatus(_proposalId) == 2 && GD.getBalanceOfMember(_memberAddress) != 0);
+        require(MR.getMemberRoleIdByAddress(_memberAddress) == PC.getRoleSequencAtIndex(GD.getProposalCategory(_proposalId),currentVotingId) && VT.getVoteId_againstMember(_memberAddress,_proposalId) == 0 );
+        
+        uint8 paramInt; uint8 paramBytes32; uint8 paramAddress;
+        (,,,,paramInt,paramBytes32,paramAddress,,) = PC.getCategoryDetails(GD.getProposalCategory(_proposalId));
+
+        if(paramInt == _paramInt.length && paramBytes32 == _paramBytes32.length && paramAddress == _paramAddress.length)
+        {
+            addVerdictOptionSVT1(_proposalId,_memberAddress,_GBTPayableTokenAmount,_optionDescHash);
+            addVerdictOptionSVT2(_proposalId,GD.getProposalCategory(_proposalId),_paramInt,_paramBytes32,_paramAddress);
+        } 
+    }
+
+    function addVerdictOptionSVT1(uint _proposalId,address _memberAddress,uint _GBTPayableTokenAmount,string _optionDescHash) internal
+    {
+        GD.setOptionAddressAndStake(_proposalId,_memberAddress,_GBTPayableTokenAmount,setOptionValue_givenByMemberSVT(_memberAddress,_proposalId,_GBTPayableTokenAmount),_optionDescHash);
+    }
+    function addVerdictOptionSVT2(uint _proposalId,uint _categoryId,uint[] _paramInt,bytes32[] _paramBytes32,address[] _paramAddress) internal
+    {
+        G1=Governance(G1Address);
+        G1.setProposalCategoryParams(_categoryId,_proposalId,_paramInt,_paramBytes32,_paramAddress,GD.getTotalVerdictOptions(_proposalId)+1); // MAKE +1 IN go
+    }
+    
+    function closeProposalVoteSVT(address _memberAddress,uint _votingType,uint _proposalId) onlyInternal
+    {
+        GD=GovernanceData(GDAddress);
+        MR=MemberRoles(MRAddress);
+        PC=ProposalCategory(PCAddress);
+        G1=Governance(G1Address);
+
+        if(_votingType == 0)
+        {
+           SV = SimpleVoting(SVAddress);
+           VT = SV;
+        }
+        else if(_votingType == 1)
+        {
+            RB = RankBasedVoting(RBAddress);
+            VT = RB;
+        } 
+        else
+        {
+            FW = FeatureWeighted(FWAddress);
+            VT = FW;
+        }
+
+        uint8 currentVotingId; uint8 category;
+        (,category,currentVotingId,,,) = GD.getProposalDetailsById2(_proposalId);
+        uint8 verdictOptions = GD.getTotalVerdictOptions(_proposalId);
+    
+        require(G1.checkProposalVoteClosing(_proposalId)==1);
+
+        uint8 max; uint totalVotes; uint verdictVal; uint majorityVote;
+        uint roleId = MR.getMemberRoleIdByAddress(_memberAddress);
+
+        max=0;  
+        for(uint8 i = 0; i < verdictOptions; i++)
+        {
+            totalVotes = SafeMath.add(totalVotes,VT.getVotesbyOption_againstProposal(_proposalId,roleId,i)); 
+            if(VT.getVotesbyOption_againstProposal(_proposalId,roleId,max) < VT.getVotesbyOption_againstProposal(_proposalId,roleId,i))
+            {  
+                max = i; 
+            }
+        }
+        verdictVal = VT.getVotesbyOption_againstProposal(_proposalId,roleId,max);
+        majorityVote= PC.getRoleMajorityVote(category,currentVotingId);
+       
+        if(SafeMath.div(SafeMath.mul(verdictVal,100),totalVotes)>=majorityVote)
+        {   
+            currentVotingId = currentVotingId+1;
+            if(max > 0 )
+            {
+                if(currentVotingId < PC.getRoleSequencLength(category))
+                {
+                    GD.updateProposalDetails(_proposalId,currentVotingId,max,0);
+                    P1=Pool(P1Address);
+                    P1.closeProposalOraclise(_proposalId,PC.getClosingTimeByIndex(category,currentVotingId));
+                } 
+                else
+                {
+                    GD.updateProposalDetails(_proposalId,currentVotingId,max,max);
+                    GD.changeProposalStatus(_proposalId,3);
+                    PC.actionAfterProposalPass(_proposalId ,category);
+                    VT.giveReward_afterFinalDecision(_proposalId);
+                }
+            }
+            else
+            {
+                GD.updateProposalDetails(_proposalId,currentVotingId,max,max);
+                GD.changeProposalStatus(_proposalId,4);
+                GD.changePendingProposalStart();
+            }      
+        } 
+        else
+        {
+            GD.updateProposalDetails(_proposalId,currentVotingId,max,max);
+            GD.changeProposalStatus(_proposalId,5);
+            GD.changePendingProposalStart();
+        } 
+    }
+}
+
+
+
+
+
+
