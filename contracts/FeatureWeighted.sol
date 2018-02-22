bbe3fb1904b8f5e3b363717624ce87ddd3f47c2e
Shiksha <shiksha@dev.somish.com>
2018-02-22 18:38:01 +0530

    Changes done while using another contract's function - Feature Weighted

--- contracts/FeatureWeighted.sol
+++ contracts/FeatureWeighted.sol
@@ -309,9 +309,9 @@ contract FeatureWeighted is VotingType
 
         (,,,,finalVerdict,) = GD.getProposalDetailsById2(_proposalId);
 
-        for(uint i=0; i<GD.getTotalVoteLengthAgainstProposal(_proposalId); i++)
+        for(uint i=0; i<GD.getVoteLengthById(_proposalId); i++)
         {
-            uint voteid = GD.getVoteIdByProposalId(_proposalId,i);
+            uint voteid = GD.getVoteIdById(_proposalId,i);
             if(allVotes[voteid].optionChosen[0] == finalVerdict)
             {
                 voteValueFavour = SafeMath.add(voteValueFavour,allVotes[voteid].voteValue);
@@ -323,7 +323,7 @@ contract FeatureWeighted is VotingType
                 G1.transferBackGBTtoken(allVotes[voteid].voter,returnTokens);
             }
 
-            // uint voteid = GD.getVoteIdByProposalId(_proposalId,i);
+            // uint voteid = GD.getVoteIdById(_proposalId,i);
             // for(uint j=0; j<allVotes[voteid].verdictChosen.length; j=i+_featureLength+1)
             // {
             //     if(allVotes[voteid].verdictChosen[j] == finalVerdict)
@@ -374,9 +374,9 @@ contract FeatureWeighted is VotingType
             G1.transferBackGBTtoken(GD.getOptionAddressByProposalId(_proposalId,finalVerdict),transferToken);
         }
 
-        for(uint i=0; i<GD.getTotalVoteLengthAgainstProposal(_proposalId); i++)
+        for(uint i=0; i<GD.getVoteLengthById(_proposalId); i++)
         {
-            uint voteid = GD.getVoteIdByProposalId(_proposalId,i);
+            uint voteid = GD.getVoteIdById(_proposalId,i);
 
             // if(getOptionValue(voteid,_proposalId,finalVerdict) != 0)
             // {
