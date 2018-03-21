b2c29dca24749433c333ddcf55bdce4ec52dd3df
tech-somish <contact@somish.com>
2018-03-21 17:37:06 +0530

    Changes in function when buying GBT

--- contracts/GBTController.sol
+++ contracts/GBTController.sol
@@ -15,7 +15,6 @@
     
 pragma solidity ^0.4.8;
 
-import "./Master.sol";
 import "./GBTStandardToken.sol";
 
 contract GBTController {
@@ -24,80 +23,68 @@ contract GBTController {
     // event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     // event Burn(address indexed _of,bytes16 eventName , uint coverId ,uint tokens);
     address public GBTStandardTokenAddress;
-    address masterAddress;
     address public owner;
-    Master MS;
     GBTStandardToken GBTS;
     uint tokenPrice;
 
     function changeMasterAddress(address _masterContractAddress) 
     {
-        if(masterAddress == 0x000)
-            masterAddress = _masterContractAddress;
-        else
-        {
-            MS=Master(masterAddress);
-            require(MS.isInternal(msg.sender) == 1);
-                masterAddress = _masterContractAddress;
-        }
+        // if(masterAddress == 0x000)
+        //     masterAddress = _masterContractAddress;
+        // else
+        // {
+        //     MS=Master(masterAddress);
+        //     require(MS.isInternal(msg.sender) == 1);
+        //         masterAddress = _masterContractAddress;
+        // }
     }
 
-    modifier onlyInternal {
-        MS=Master(masterAddress);
-            require(MS.isInternal(msg.sender) == 1);
-        _; 
-    }
-    modifier onlyOwner{
-        MS=Master(masterAddress);
-            require(MS.isOwner(msg.sender) == 1);
-        _; 
-    }
-
+   
     function GBTController() 
     {
         owner = msg.sender;
         tokenPrice = 1*10**15;
     }
 
-    function changeGBTtokenAddress(address _Address) onlyInternal
+    function changeGBTtokenAddress(address _Address) 
     {
         GBTStandardTokenAddress = _Address;
     }
 
-    function transferGBT(address _to, uint256 _value,string _description) onlyInternal
+    function transferGBT(address _to, uint256 _value,string _description) 
     {
         GBTS=GBTStandardToken(GBTStandardTokenAddress);
 
         require(_value <= GBTS.balanceOf(address(this)));
         GBTS.addInBalance(_to,_value);
         GBTS.subFromBalance(address(this),_value);
-        GBTS.callTransferEvent(address(this), _to, _value, _description);
+        GBTS.callTransferGBTEvent(address(this), _to, _value, _description);
     }
     
-    function receiveGBT(address _from,uint _value, string _description) onlyInternal
+    function receiveGBT(address _from,uint _value, string _description) 
     {
         GBTS=GBTStandardToken(GBTStandardTokenAddress);
 
         require(_value <= GBTS.balanceOf(_from));
         GBTS.addInBalance(address(this),_value);
         GBTS.subFromBalance(_from,_value);
-        GBTS.callTransferEvent(_from, address(this), _value, _description);
+        GBTS.callTransferGBTEvent(_from, address(this), _value, _description);
     }  
     
     uint public actual_amount;
     
-    function buyTokenGBT(address _to,string _description) payable 
+    function buyTokenGBT(address _to) payable 
     {
         actual_amount = (msg.value/tokenPrice);  // amount that was sent          
-        rewardToken(_to,actual_amount,_description);
+        rewardToken(_to,actual_amount);
     }
 
-    function rewardToken(address _to,uint _amount,string _description)  internal  
+    function rewardToken(address _to,uint _amount)    
     {
         GBTS=GBTStandardToken(GBTStandardTokenAddress);
         GBTS.addInBalance(_to,_amount);
         GBTS.addInTotalSupply(_amount);
-        GBTS.callTransferEvent(GBTStandardTokenAddress, _to, _amount, _description);
+        GBTS.callTransferGBTEvent(GBTStandardTokenAddress, _to, _amount, "GBT Purchased");
     }
 
     function changeTokenPrice(uint _price)
