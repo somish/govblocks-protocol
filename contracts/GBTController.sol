5ba1271c272b6a6be2e18958f6d305f0ed295c16
nitika-goel <nitika.goel@somish.com>
2018-04-14 11:33:09 +0530

    Added a few functions for security purpose when receiving stake from user

--- contracts/GBTController.sol
+++ contracts/GBTController.sol
@@ -39,6 +39,7 @@ contract GBTController
     ProposalCategory PC;
     uint public tokenPrice;
     uint public actual_amount;
+    uint public tokenHoldingTime;
 
     modifier onlyGBM
     {
@@ -119,6 +120,38 @@ contract GBTController
         return tokenPrice;
     }
 
+    function getLockedTokenId(bytes32 _gbUserName,uint _proposalId)constant returns(uint id)
+    {
+        id = proposal_lockToken[_gbUserName][_proposalId];
+    }
+
+    function getLockedAmountMyId(uint _id)constant returns (uint stake)
+    {
+        stake = lockToken[_id];
+    }
+
+    function lockMemberToken(bytes32 _gbUserName,uint _memberStake,uint _proposalId,uint _tokenlockPerc)
+    {
+        GBM=GovBlocksMaster(GBMAddress);
+        address master = GBM.getDappMasterAddress(_gbUserName);address PCAddress; uint category; uint currVotingId;
+        MS=Master(master);
+        uint versionNo = MS.versionLength()-1; 
+        (,GDAddress) = MS.allContractVersions(versionNo,1);
+        (,PCAddress) = MS.allContractVersions(versionNo,3); uint length; uint pClosingTime;
+        (,category,currVotingId,,,) = GD.getProposalDetailsById2(_proposalId);
+        (,length) = PC.getClosingTimeLength(category);
+        
+        uint id = getLockedTokenId(_gbUserName,_proposalId);
+        lockToken.push((stake*_tokenlockPerc)/100,totalTime);
+        proposal_lockToken[_gbUserName][_proposalId] = id;
+        user_lockToken[msg.sender] = id;
+    }
+
+    function setTokenHoldingTime(uint _newValidity)
+    {
+        tokenHoldingTime = _newValidity;
+    }
+
    function openProposalForVoting(bytes32 _gbUserName,uint _proposalId,uint _TokenAmount) public
    {
         GBM=GovBlocksMaster(GBMAddress);
@@ -130,9 +163,14 @@ contract GBTController
 
         G1=Governance(G1Address);
         GD=governanceData(GDAddress);
-        receiveGBT(_TokenAmount,"Payable GBT Stake to submit proposal for voting");
-        receiveGBT(GD.getProposalIncentive(_proposalId),"Dapp incentive to be distributed in GBT");
-        G1.setProposalValue(_proposalId,_TokenAmount);
+        // receiveGBT(_TokenAmount,"Payable GBT Stake to submit proposal for voting");
+        // receiveGBT(GD.getProposalIncentive(_proposalId),"Dapp incentive to be distributed in GBT");
+        lockMemberToken(_gbUserName,_proposalId,_TokenAmount,PC.getLockPercProposal(GD.getProposalCategory(_proposalId)));
+        uint depositAmount = ((_TokenAmount*GD.burnPercProposal())/100);
+        uint finalAmount = depositAmount + GD.getDepositTokensByAddress(msg.sender,_proposalId);
+        GD.setDepositTokens(msg.sender,_proposalId,finalAmount);
+        // G1.setProposalValue(_proposalId,_TokenAmount);
+        GD.setProposalStake(_proposalId,_memberStake);
         G1.openProposalForVoting(_proposalId,msg.sender,GD.getProposalCategory(_proposalId));
    }
 
@@ -146,7 +184,7 @@ contract GBTController
         (,PCAddress) = MS.allContractVersions(versionNo,3);
         PC=ProposalCategory(PCAddress);
         receiveStakeInGbt(_TokenAmount,PC.getCatIncentive(_categoryId));
-    
+
         G1=Governance(G1Address); 
         G1.createProposalwithOption(_proposalDescHash,msg.sender,_TokenAmount,_votingTypeId,_categoryId,_optionHash);
    }
@@ -154,10 +192,12 @@ contract GBTController
    function submitProposalWithOption(bytes32 _gbUserName,uint _proposalId,uint _TokenAmount,string _optionHash) public
    {
         GBM=GovBlocksMaster(GBMAddress);
-        address master = GBM.getDappMasterAddress(_gbUserName); address G1Address;
+        address master = GBM.getDappMasterAddress(_gbUserName); address G1Address;address PCAddress;
         MS=Master(master);
         uint versionNo = MS.versionLength()-1; 
         (,G1Address) = MS.allContractVersions(versionNo,8);
+        (,PCAddress) = MS.allContractVersions(versionNo,3);
+        PC=ProposalCategory(PCAddress);
         receiveStakeInGbt(_TokenAmount,GD.getProposalIncentive(_proposalId));
 
         G1=Governance(G1Address);
@@ -166,23 +206,39 @@ contract GBTController
 
     function receiveStakeInGbt(uint _TokenAmount,uint _Incentive) internal
     {
-        uint gbtTransfer = SafeMath.div(_TokenAmount,2);
-        receiveGBT(gbtTransfer,"Payable GBT Stake to submit proposal for voting");
+        uint gbtTransfer = SafeMath.div(_TokenAmount,2); uint depositAmount;uint finalAmount;
+        // receiveGBT(gbtTransfer,"Payable GBT Stake to submit proposal for voting");
         uint amount = _TokenAmount - gbtTransfer;
-        receiveGBT(amount,"Payable GBT Stake for adding solution against proposal");
-        receiveGBT(_Incentive,"Dapp incentive to be distributed in GBT");
+        // receiveGBT(amount,"Payable GBT Stake for adding solution against proposal");
+        // receiveGBT(_Incentive,"Dapp incentive to be distributed in GBT");
+
+        lockMemberToken(_gbUserName,_proposalId,gbtTransfer,PC.getLockPercProposal(GD.getProposalCategory(_proposalId)));
+        depositAmount = ((gbtTransfer*GD.burnPercProposal())/100);
+        finalAmount = depositAmount + GD.getDepositTokensByAddress(msg.sender,_proposalId);
+        GD.setDepositTokens(msg.sender,_proposalId,finalAmount);
+
+        lockMemberToken(_gbUserName,_proposalId,amount,PC.getLockPercOption(GD.getProposalCategory(_proposalId)));
+        depositAmount = ((amount*GD.burnPercOption())/100);
+        finalAmount = depositAmount + GD.getDepositTokensByAddress(msg.sender,_proposalId);
+        GD.setDepositTokens(msg.sender,_proposalId,finalAmount);
     }
 
     function initiateVerdictOption(bytes32 _gbUserName,uint _proposalId,uint _GBTPayableTokenAmount,string _optionHash,uint _dateAdd) public
     {
         GBM=GovBlocksMaster(GBMAddress);
-        address master = GBM.getDappMasterAddress(_gbUserName); address GDAddress;
+        address master = GBM.getDappMasterAddress(_gbUserName); address GDAddress;address PCAddress;
         MS=Master(master);
         uint versionNo = MS.versionLength()-1; 
         (,GDAddress) = MS.allContractVersions(versionNo,1);
+        (,PCAddress) = MS.allContractVersions(versionNo,3);
+        PC=ProposalCategory(PCAddress);
         GD=governanceData(GDAddress);
 
-        receiveGBT(_GBTPayableTokenAmount,"Payable GBT Stake for adding solution against proposal");
+        // receiveGBT(_GBTPayableTokenAmount,"Payable GBT Stake for adding solution against proposal");
+        lockMemberToken1(_gbUserName,_proposalId,gbtTransfer,PC.getLockPercOption(GD.getProposalCategory(_proposalId)));
+        uint depositAmount = ((gbtTransfer*GD.burnPercOption())/100);
+        GD.setDepositTokens(msg.sender,_proposalId,depositAmount);
+
         VT=VotingType(GD.getProposalVotingType(_proposalId));
         VT.initiateVerdictOption(_proposalId,msg.sender,_GBTPayableTokenAmount,_optionHash,_dateAdd);
     }
@@ -190,13 +246,16 @@ contract GBTController
     function proposalVoting(bytes32 _gbUserName,uint _GBTPayableTokenAmount,uint _proposalId,uint[] _optionChosen) public
     {
         GBM=GovBlocksMaster(GBMAddress);
-        address master = GBM.getDappMasterAddress(_gbUserName); address GDAddress;
+        address master = GBM.getDappMasterAddress(_gbUserName); address GDAddress;address PCAddress;
         MS=Master(master);
         uint versionNo = MS.versionLength()-1; 
         (,GDAddress) = MS.allContractVersions(versionNo,1);
         GD=governanceData(GDAddress);
+        (,PCAddress) = MS.allContractVersions(versionNo,3);
+        PC=ProposalCategory(PCAddress);
 
-        receiveGBT(_GBTPayableTokenAmount,"Payable GBT Stake for voting against proposal");
+        // receiveGBT(_GBTPayableTokenAmount,"Payable GBT Stake for voting against proposal");
+        lockMemberToken(_gbUserName,_proposalId,_TokenAmount,PC.getLockPercVote(GD.getProposalCategory(_proposalId))););
         VT=VotingType(GD.getProposalVotingType(_proposalId));
         VT.proposalVoting(_proposalId,_optionChosen,msg.sender,_GBTPayableTokenAmount);
     }
