90a2b9fff87c36dcdc8c6e3112a6b1cba4e6a714
Shiksha <shiksha@dev.somish.com>
2018-06-29 17:18:55 +0530

    Pool linting

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -22,50 +22,49 @@ import "./Upgradeable.sol";
 import "./usingOraclize.sol";
 // import "./oraclizeAPI_0.4.sol";
 
+
 contract Pool is usingOraclize, Upgradeable {
-    event closeProposal(uint256 indexed proposalId, uint256 closingTime, string URL);
-    event apiresult(address indexed sender, string msg, bytes32 myid);
+    event CloseProposal(uint256 indexed proposalId, uint256 closingTime, string url);
+    event ApiResult(address indexed sender, string msg, bytes32 myid);
     using SafeMath for uint;
 
-    struct apiId {
-        bytes8 type_of;
+    struct ApiId {
+        bytes8 typeOf;
         uint proposalId;
         uint64 dateAdd;
         uint64 dateUpd;
     }
 
-    mapping(bytes32 => apiId) public allAPIid;
+    mapping(bytes32 => ApiId) public allAPIid;
     bytes32[] public allAPIcall;
-    address masterAddress;
-    Master MS;
-    GBTStandardToken GBTS;
+    address public masterAddress;
+    Master private master;
+    GBTStandardToken private gbt;
 
-    function () payable {}
+    function () public payable {}
 
     /// @dev Changes master address
     /// @param _add New master address
-    function changeMasterAddress(address _add) {
+    function changeMasterAddress(address _add) public {
         if (masterAddress == 0x000)
             masterAddress = _add;
         else {
-            MS = Master(masterAddress);
-            if (MS.isInternal(msg.sender) == true)
-                masterAddress = _add;
-            else
-                throw;
+            master = Master(masterAddress);
+            require(master.isInternal(msg.sender));
+            masterAddress = _add;
         }
 
     }
 
     modifier onlyInternal {
-        MS = Master(masterAddress);
-        require(MS.isInternal(msg.sender) == true);
+        master = Master(masterAddress);
+        require(master.isInternal(msg.sender) == true);
         _;
     }
 
     modifier onlyOwner {
-        MS = Master(masterAddress);
-        require(MS.isOwner(msg.sender) == true);
+        master = Master(masterAddress);
+        require(master.isOwner(msg.sender) == true);
         _;
     }
 
@@ -76,64 +75,79 @@ contract Pool is usingOraclize, Upgradeable {
 
     /// @dev Changes GBT standard token address
     /// @param _GBTSAddress New GBT standard token address
-    function changeGBTSAddress(address _GBTSAddress) onlyMaster {
-        GBTS = GBTStandardToken(_GBTSAddress);
+    function changeGBTSAddress(address _gbtAddress) public onlyMaster {
+        gbt = GBTStandardToken(_gbtAddress);
     }
 
     /// @dev just to adhere to the interface
     function updateDependencyAddresses() public {
-        MS = Master(masterAddress);
-        GBTS = GBTStandardToken(MS.getLatestAddress("GS"));
+        master = Master(masterAddress);
+        gbt = GBTStandardToken(master.getLatestAddress("GS"));
     }
 
     /// @dev converts pool ETH to GBT
     /// @param _gbt number of GBT to buy
     function buyPoolGBT(uint _gbt) {
-        uint _wei = SafeMath.mul(_gbt, GBTS.tokenPrice());
-        GBTS.buyToken.value(_wei)();
+        uint _wei = SafeMath.mul(_gbt, gbt.tokenPrice());
+        gbt.buyToken.value(_wei)();
     }
 
     /// @dev Closes Proposal voting using oraclize once the time is over.
     /// @param _proposalId Proposal id
     /// @param _closingTime Remaining Closing time of proposal
-    function closeProposalOraclise(uint _proposalId, uint _closingTime) {
-        uint index = getApiCall_length();
+    function closeProposalOraclise(uint _proposalId, uint _closingTime) public {
+        uint index = getApiCalllength();
         bytes32 myid2;
-        MS = Master(masterAddress);
+        master = Master(masterAddress);
 
         if (_closingTime == 0)
-            myid2 = oraclize_query("URL", strConcat("http://a1.govblocks.io/closeProposalVoting.js/42/", bytes32ToString(MS.DappName()), "/", uint2str(index)));
+            myid2 = 
+                oraclize_query(
+                    "URL", 
+                    strConcat(
+                        "http://a1.govblocks.io/closeProposalVoting.js/42/", 
+                        bytes32ToString(master.DappName()), 
+                        "/", 
+                        uint2str(index)
+                    )
+                );
         else
-            myid2 = oraclize_query(_closingTime, "URL", strConcat("http://a1.govblocks.io/closeProposalVoting.js/42/", bytes32ToString(MS.DappName()), "/", uint2str(index)));
+            myid2 = 
+                oraclize_query(
+                    _closingTime, 
+                    "URL", 
+                    strConcat(
+                        "http://a1.govblocks.io/closeProposalVoting.js/42/", 
+                        bytes32ToString(master.DappName()), 
+                        "/", 
+                        uint2str(index)
+                    )
+                );
 
         uint closeTime = now + _closingTime;
-        closeProposal(_proposalId, closeTime, strConcat("http://a1.govblocks.io/closeProposalVoting.js/42/", bytes32ToString(MS.DappName()), "/", uint2str(index)));
+        CloseProposal(
+            _proposalId, 
+            closeTime, 
+            strConcat(
+                "http://a1.govblocks.io/closeProposalVoting.js/42/", 
+                bytes32ToString(master.DappName()), 
+                "/", 
+                uint2str(index)
+            )
+        );
         saveApiDetails(myid2, "PRO", _proposalId);
         addInAllApiCall(myid2);
     }
 
     /// @dev Get total length of oraclize call being triggered using this function  "closeProposalOraclise"
-    function getApiCall_length() constant returns(uint len) {
+    function getApiCallLength() public constant returns(uint len) {
         return allAPIcall.length;
-    }
-
-    /// @dev Saves api details
-    /// @param myid Proposal id
-    /// @param _typeof typeOf differ in case we have different stages of process. i.e. here default typeOf is "PRO"
-    /// @param id This is index of the oraclize call.
-    function saveApiDetails(bytes32 myid, bytes8 _typeof, uint id) internal {
-        allAPIid[myid] = apiId(_typeof, id, uint64(now), uint64(now));
-    }
-
-    /// @dev Adds api response hash returned in all api call
-    function addInAllApiCall(bytes32 myid) internal {
-        allAPIcall.push(myid);
     }
 
     /// @dev Gets api call of index
     /// @param index Index to call
     /// @return myid Id with respect to index
-    function getApiCall_Index(uint index) constant returns(bytes32 myid) {
+    function getApiCallIndex(uint index) public constant returns(bytes32 myid) {
         myid = allAPIcall[index];
     }
 
@@ -143,21 +157,25 @@ contract Pool is usingOraclize, Upgradeable {
     /// @return id Id of api
     /// @return dateAdd Date proposal was added 
     /// @return dateUpd Date proposal was updated
-    function getApiCallDetails(bytes32 myid) constant returns(bytes8 _typeof, uint id, uint64 dateAdd, uint64 dateUpd) {
-        return (allAPIid[myid].type_of, allAPIid[myid].proposalId, allAPIid[myid].dateAdd, allAPIid[myid].dateUpd);
+    function getApiCallDetails(bytes32 myid) 
+        public 
+        constant 
+        returns(bytes8 _typeof, uint id, uint64 dateAdd, uint64 dateUpd) 
+    {
+        return (allAPIid[myid].typeOf, allAPIid[myid].proposalId, allAPIid[myid].dateAdd, allAPIid[myid].dateUpd);
     }
 
     /// @dev Gets type of proposal wrt api id
     /// @param myid Id of api
     /// @return _typeof Type of proposal
-    function getApiIdTypeOf(bytes32 myid) constant returns(bytes16 _typeof) {
-        _typeof = allAPIid[myid].type_of;
+    function getApiIdTypeOf(bytes32 myid) public constant returns(bytes16 _typeof) {
+        _typeof = allAPIid[myid].typeOf;
     }
 
     /// @dev Gets proposal id of api id
     /// @param myid Api id
     /// @return id1 Proposal id
-    function getProposalIdOfApiId(bytes32 myid) constant returns(uint id1) {
+    function getProposalIdOfApiId(bytes32 myid) public constant returns(uint id1) {
         id1 = allAPIid[myid].proposalId;
     }
 
@@ -165,21 +183,21 @@ contract Pool is usingOraclize, Upgradeable {
     /// @param myid Api id
     /// @param res Result string
     function __callback(bytes32 myid, string res) public {
-        MS = Master(masterAddress);
-        if (msg.sender != oraclize_cbAddress() && MS.isOwner(msg.sender) != true) throw;
+        master = Master(masterAddress);
+        require(msg.sender != oraclize_cbAddress() && master.isOwner(msg.sender));
         allAPIid[myid].dateUpd = uint64(now);
     }
 
     /// @dev Transfer Ether back to Pool    
     /// @param amount Amount to be transferred back
-    function transferBackEther(uint256 amount) onlyOwner {
+    function transferBackEther(uint256 amount) public onlyOwner {
         msg.sender.transfer(amount);
     }
 
     /// @dev Byte32 to string
     /// @param x Byte32 to be converted to string
     /// @return bytesStringTrimmed Resultant string 
-    function bytes32ToString(bytes32 x) constant returns(string) {
+    function bytes32ToString(bytes32 x) public constant returns(string) {
         bytes memory bytesString = new bytes(32);
         uint charCount = 0;
         for (uint j = 0; j < 32; j++) {
@@ -198,14 +216,27 @@ contract Pool is usingOraclize, Upgradeable {
 
     /// @dev Gets proposal index by proposal id
     /// @return myIndexId Api index of corresponding proposal id
-    function getMyIndexByProposalId(uint _proposalId) constant returns(uint myIndexId) {
-        uint length = getApiCall_length();
+    function getMyIndexByProposalId(uint _proposalId) public constant returns(uint myIndexId) {
+        uint length = getApiCallLength();
         for (uint i = 0; i < length; i++) {
-            bytes32 myid = getApiCall_Index(i);
+            bytes32 myid = getApiCallIndex(i);
             uint propId = getProposalIdOfApiId(myid);
             if (_proposalId == propId)
                 myIndexId = i;
         }
+    }
+
+    /// @dev Saves api details
+    /// @param myid Proposal id
+    /// @param _typeof typeOf differ in case we have different stages of process. i.e. here default typeOf is "PRO"
+    /// @param id This is index of the oraclize call.
+    function saveApiDetails(bytes32 myid, bytes8 _typeof, uint id) internal {
+        allAPIid[myid] = ApiId(_typeof, id, uint64(now), uint64(now));
+    }
+
+    /// @dev Adds api response hash returned in all api call
+    function addInAllApiCall(bytes32 myid) internal {
+        allAPIcall.push(myid);
     }
 
 }
