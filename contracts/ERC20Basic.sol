620a13ced748f94b3f94af3c3e92d2c891a271c4
Mudit Gupta <guptamudit@ymail.com>
2018-07-16 11:41:20 +0530

    Token changes

--- contracts/ERC20Basic.sol
+++ contracts/ERC20Basic.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.4.24;
  * See https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
+  //function totalSupply() public view returns (uint256); totalSupply exisits as a public variable in GBT
   function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
---
+++ contracts/GenerateMintableTokens.sol
@@ -0,0 +1,34 @@
+/* Copyright (C) 2017 GovBlocks.io
+
+  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ */
+
+pragma solidity ^0.4.24;
+
+import "./MintableToken.sol";
+
+contract GenerateMintableTokens
+{
+    // create governanceData Contract
+    mapping(bytes32=>address) contractAddress;
+    function getAddress(bytes32 _gbUserName)constant returns(address)
+    {
+        return (contractAddress[_gbUserName]);
+    }
+  
+    function GenerateContract(bytes32 _gbUserName,string _name,address _owner)
+    {
+       contractAddress[_gbUserName] = new MintableToken(_name,_owner);
+    }
+  
+}
