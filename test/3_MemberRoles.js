3ae54a693f1d97d223e877e93b8e57d79cf668d7
Mudit Gupta <guptamudit@ymail.com>
2018-08-03 15:46:11 +0530

    GBM change descHash added

--- test/3_MemberRoles.js
+++ test/3_MemberRoles.js
@@ -8,17 +8,11 @@ contract('MemberRoles', function([owner,member]) {
     });
     assert.equal(1, 1, "something is not right");
   });
-  // without this "it", the test sometimes skips to next test before the first test is completed.
-  // first test sets the insance and hence is required to be completed before all others.
-  // before, beforeAll are not working for some reason, hence, this temp fix is used.
-  it("should should have the instance", async function () {
-    assert.equal(1, 1, "something is not right");
-  });
   it("should be initialized with default roles", async function () {
     this.timeout(100000);
     let ab = await mr.getMemberRoleNameById(1);
     let th = await mr.getMemberRoleNameById(2);
-    assert.equal(await mr.constructorCheck(), true, "constructorCheck wasn't false");
+    assert.equal(await mr.constructorCheck(), true, "constructorCheck wasn't true");
     assert.equal(ab.toString(), "Advisory Board", "Advisory Board not created");
     assert.equal(th.toString(), "Token Holder", "Token Holder not created");
     assert.equal(await mr.checkRoleIdByAddress(owner,1), true, "Owner not added to AB");
