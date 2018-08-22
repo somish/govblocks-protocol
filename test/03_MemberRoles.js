1cb5a161e0fc70634901d621d9974d3ad45e67c9
Mudit Gupta <guptamudit@ymail.com>
2018-08-22 14:47:01 +0530

    Updated member role tests for better coverage

--- test/03_MemberRoles.js
+++ test/03_MemberRoles.js
@@ -1,9 +1,9 @@
 const MemberRoles = artifacts.require('MemberRoles');
+const catchRevert = require('../helpers/exceptions.js').catchRevert;
 let mr;
 
 // addMemberRole tested already
-contract('MemberRoles', ([owner, member]) => {
-  
+contract('MemberRoles', ([owner, member, other]) => {
   before(() => {
     MemberRoles.deployed().then((instance) => {
       mr = instance;
@@ -12,6 +12,7 @@ contract('MemberRoles', ([owner, member]) => {
 
   it('should be initialized with default roles', async function() {
     this.timeout(100000);
+    await catchRevert(mr.memberRolesInitiate('0x41', owner, owner));
     const ab = await mr.getMemberRoleNameById.call(1);
     const th = await mr.getMemberRoleNameById(2);
 
@@ -32,26 +33,40 @@ contract('MemberRoles', ([owner, member]) => {
   it('Should check getters', async function() {
     this.timeout(100000);
     const g3 = await mr.getAllAddressByRoleId(1);
+    assert.equal(g3[1][0], owner);
     const g4 = await mr.getAllMemberLength(1);
+    assert.equal(g4.toNumber(), 2);
     const g5 = await mr.getAllMemberAddressById(1, 0);
+    assert.equal(g5, owner);
     const g6 = await mr.getRolesAndMember();
-    await mr.changeMasterAddress();
-    await mr.updateDependencyAddresses();
-
-    // TODO verify the data returned
+    await mr.changeMasterAddress(); // just for interface, they do nothing
+    await mr.updateDependencyAddresses(); // just for interface, they do nothing
   });
 
   it('Should change validity of member', async function() {
     this.timeout(100000);
     await mr.setRoleValidity(1, true);
     await mr.setValidityOfMember(member, 1, 5);
+    const mrs22 = await mr.getAllAddressByRoleId(1);
     const val = await mr.getValidity(member, 1);
     assert.equal(val.toNumber(), 5, 'Validity not updated');
+    await mr.updateMemberRole(member, 1, true, 356000000000854);
+    assert.equal(await mr.checkRoleIdByAddress(member, 1), true, 'user not added to AB');
+    await mr.updateMemberRole(member, 1, false, 0);
+    assert.equal(await mr.checkRoleIdByAddress(member, 1), false, 'user not removed from AB');
   });
 
   it('Should change can add member', async function() {
     this.timeout(100000);
     await mr.changeCanAddMember(1, member);
+    await catchRevert(mr.updateMemberRole(member, 1, true, 356854, {from: other}));
     assert.equal(await mr.getAuthrizedMemberAgainstRole(1), member, 'Authorized address not changed');
+  });
+
+  it('Should get proper Roles', async () => {
+    const mrs = await mr.getRoleIdByAddress(owner);
+    assert.equal(mrs[0], 1);
+    const mrs2 = await mr.getRoleIdByAddress(other);
+    assert.equal(await mr.checkRoleIdByAddress(owner, 0), true);
   });
 });
--- test/07_Governance.js
+++ test/07_Governance.js
@@ -4,30 +4,34 @@ const catchRevert = require('../helpers/exceptions.js').catchRevert;
 const encode = require('../helpers/encoder.js').encode;
 const Pool = artifacts.require('Pool');
 const SimpleVoting = artifacts.require('SimpleVoting');
+const Master = artifacts.require('Master');
 const GBTStandardToken = artifacts.require('GBTStandardToken');
 let gbt;
 let sv;
 let pl;
 let gd;
 let gv;
+let ms;
 
-contract('Governance', function([owner, notOwner]) {
-
-  before(function() {
-    Governance.deployed().then(function(instance) {
+contract('Governance', ([owner, notOwner]) => {
+  before(() => {
+    Governance.deployed().then((instance) => {
       gv = instance;
       return GovernanceData.deployed();
-    }).then(function(instance) {
+    }).then((instance) => {
       gd = instance;
       return Pool.deployed();
-    }).then(function(instance) {
+    }).then((instance) => {
       pl = instance;
       return SimpleVoting.deployed();
-    }).then(function(instance) {
+    }).then((instance) => {
       sv = instance;
       return GBTStandardToken.deployed();
-    }).then(function(instance) {
+    }).then((instance) => {
       gbt = instance;
+      return Master.deployed();
+    }).then((instance) => {
+      ms = instance;
     });
   });
 
@@ -47,14 +51,14 @@ contract('Governance', function([owner, notOwner]) {
     p = await gd.getAllProposalIdsLengthByAddress(owner);
     p = p.toNumber() - 1;
     await catchRevert(gv.openProposalForVoting(p));
-    await gv.categorizeProposal(p, 8);
-    let category = await gd.getProposalCategory(p);
-    assert.equal(category.toNumber(), 8, 'Category not set properly');
+    await gv.categorizeProposal(p, 9);
+    const category = await gd.getProposalCategory(p);
+    assert.equal(category.toNumber(), 9, 'Category not set properly');
   });
 
   it('Should submit proposal with solution', async function() {
     this.timeout(100000);
-    let actionHash = encode('addNewMemberRole(bytes32,string,address,bool)', '0x41647669736f727920426f617265000000000000000000000000000000000000', 'New member role', owner, false);
+    const actionHash = encode('addNewMemberRole(bytes32,string,address,bool)', '0x41647669736f727920426f617265000000000000000000000000000000000000', 'New member role', owner, false);
     p1 = await gd.getAllProposalIdsLengthByAddress(owner);
     await catchRevert(gv.submitProposalWithSolution(p1.toNumber() - 1, 'Addnewmember', actionHash, {from: notOwner}));
     await gv.submitProposalWithSolution(p1.toNumber() - 1, 'Addnewmember', actionHash);
@@ -63,6 +67,9 @@ contract('Governance', function([owner, notOwner]) {
     await assert.isAtLeast(remainingTime.toNumber(), 1, 'Remaining time not set');
     remainingTime = await gv.getRemainingClosingTime(p1.toNumber() - 1, 0);
     await assert.isAtLeast(remainingTime.toNumber(), 1, 'Remaining time not set');
+    const g2 = await gv.getSolutionIdAgainstAddressProposal(owner, 0);
+    assert.equal(g2[0].toNumber(), 0);
+    const pr = await pl.getPendingReward(owner);
     await pl.claimReward(owner);
   });
 
@@ -70,8 +77,10 @@ contract('Governance', function([owner, notOwner]) {
     this.timeout(100000);
     p = await gd.getAllProposalIdsLengthByAddress(owner);
     p = p.toNumber() - 1;
+    const g3 = await gv.getAllVoteIdsLengthByProposal(p);
     await sv.proposalVoting(p, [1]);
-    await catchRevert(sv.proposalVoting(p, [1]));
+    const g4 = await gv.getAllVoteIdsLengthByProposal(p);
+    assert.equal(g4.toNumber(), g3.toNumber() + 1);
   });
 
   it('Should close the proposal', async function() {
@@ -80,27 +89,29 @@ contract('Governance', function([owner, notOwner]) {
     p = p.toNumber() - 1;
     await sv.closeProposalVote(p);
     await catchRevert(sv.closeProposalVote(p));
-    await pl.claimReward(owner);
   });
-  
+
   it('Should check getters', async function() {
     this.timeout(100000);
-    let g1 = await gv.getMemberDetails(owner);
-    let g2 = await gv.getSolutionIdAgainstAddressProposal(owner, 0);
-    let g3 = await gv.getAllVoteIdsLengthByProposal(0);
-    let g4 = await gv.master();
-    // TODO verify the data returned
+    const g4 = await gv.master();
+    assert.equal(g4, ms.address);
   });
 
-  it('Should create proposals with dApp token', async function() {
+  it('Should create proposals with dApp token', async () => {
     await gd.setDAppTokenSupportsLocking(true);
     p1 = await gd.getAllProposalIdsLengthByAddress(owner);
+    await gv.createProposal('Add new member', 'Add new member', 'Addnewmember', 0, 9);
     await gv.createProposal('Add new member', 'Add new member', 'Addnewmember', 0, 0);
+    await gv.categorizeProposal(p1.toNumber(), 9);
     p2 = await gd.getAllProposalIdsLengthByAddress(owner);
-    assert.equal(p1.toNumber() + 1, p2.toNumber(), 'Proposal not created');
+    assert.equal(p1.toNumber() + 2, p2.toNumber(), 'Proposal not created');
+    const pr = await pl.getPendingReward(owner);
+    await pl.claimReward(owner);
+    const g1 = await gv.getMemberDetails(owner);
+    assert.isAbove(g1[0].toNumber(), 1);
   });
 
-  it('Should not allow unauthorized people to create proposals', async function() {
+  it('Should not allow unauthorized people to create proposals', async () => {
     await catchRevert(gv.createProposal('Add new member', 'Add new member', 'Addnewmember', 0, 10, {from: notOwner}));
     await catchRevert(gv.createProposal('Add new member', 'Add new member', 'Addnewmember', 0, 8, {from: notOwner}));
     p1 = await gd.getAllProposalIdsLengthByAddress(owner);
