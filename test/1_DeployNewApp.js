73271223f68e0a3d914ff0dc2d39a2346d23b15b
Mudit Gupta <guptamudit@ymail.com>
2018-08-23 16:13:00 +0530

    Added prettier for formatting

--- test/1_DeployNewApp.js
+++ test/1_DeployNewApp.js
@@ -1,13 +1,13 @@
-var MemberRoles = artifacts.require("MemberRoles");
-var GovBlocksMaster = artifacts.require("GovBlocksMaster");
-var Master = artifacts.require("Master");
-var GBTStandardToken = artifacts.require("GBTStandardToken");
-var Governance = artifacts.require("Governance");
-var GovernanceData = artifacts.require("GovernanceData");
-var Pool = artifacts.require("Pool");
-var ProposalCategory = artifacts.require("ProposalCategory");
-var SimpleVoting = artifacts.require("SimpleVoting");
-var EventCaller = artifacts.require("EventCaller");
+var MemberRoles = artifacts.require('MemberRoles');
+var GovBlocksMaster = artifacts.require('GovBlocksMaster');
+var Master = artifacts.require('Master');
+var GBTStandardToken = artifacts.require('GBTStandardToken');
+var Governance = artifacts.require('Governance');
+var GovernanceData = artifacts.require('GovernanceData');
+var Pool = artifacts.require('Pool');
+var ProposalCategory = artifacts.require('ProposalCategory');
+var SimpleVoting = artifacts.require('SimpleVoting');
+var EventCaller = artifacts.require('EventCaller');
 var gbts;
 var gbm;
 var ec;
@@ -23,14 +23,14 @@ const json = require('./../build/contracts/Master.json');
 var bytecode = json['bytecode'];
 
 describe('Deploy new dApp', function() {
-  it("should create a new dApp", async function () {
+  it('should create a new dApp', async function() {
     this.timeout(100000);
     gbm = await GovBlocksMaster.new();
-    gbts =  await GBTStandardToken.new();
-    await gbm.govBlocksMasterInit("0x0", "0x0");
+    gbts = await GBTStandardToken.new();
+    await gbm.govBlocksMasterInit('0x0', '0x0');
     await gbm.setMasterByteCode(bytecode.substring(10000));
     await gbm.setMasterByteCode(bytecode);
-    await gbm.addGovBlocksUser("0x42", gbts.address, "descHash");
+    await gbm.addGovBlocksUser('0x42', gbts.address, 'descHash');
     gd = await GovernanceData.new();
     mr = await MemberRoles.new();
     pc = await ProposalCategory.new();
@@ -43,11 +43,9 @@ describe('Deploy new dApp', function() {
     add.push(sv.address);
     add.push(gv.address);
     add.push(pl.address);
-    let mad = await gbm.getDappMasterAddress("0x42");
+    let mad = await gbm.getDappMasterAddress('0x42');
     ms = await Master.at(mad);
     await ms.addNewVersion(add);
-    assert.equal(await ms.versionLength(), 1, "dApp version not created");
+    assert.equal(await ms.versionLength(), 1, 'dApp version not created');
   });
 });
-
-
