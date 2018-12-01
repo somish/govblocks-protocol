435d69175e7a0a70892074b00f048b41ca01d498
tech-somish <contact@somish.com>
2018-12-01 11:48:04 +0530

    updated initial category hashes

--- contracts/ProposalCategory.sol
+++ contracts/ProposalCategory.sol
@@ -150,7 +150,31 @@ contract ProposalCategory is IProposalCategory, Governed {
     function proposalCategoryInitiate(bytes32 _dAppName) external { //solhint-disable-line
         require(!constructorCheck);
         dappName = _dAppName;
-
+        addInitialCategories("Uncategorized", "QmRnwMshX2L6hTv3SgB6J6uahK7tRgPNfkt91siznLqzQX", "MR");
+        addInitialCategories("Add new member role", "QmT3sMfqAvTgCkcsdVgiHvMycEWoeoQiD86e4H744pqfhF", "MR");
+        addInitialCategories("Update member role", "QmV55gWxnEBF8reTrVKrhbg5QrwqA65kFhMEFWDnnpphrJ", "MR");
+        addInitialCategories("Add new category", "QmVXcXmr1aXeK3XSvGXAbDmhEQordetU1Z71h1zmbAZXBf", "PC");
+        addInitialCategories("Edit category", "QmXB8fB6LpkWqLjhkNYT3412z439VNyN5tRxPV6JTyEHKu", "PC");
+        addInitialCategories("Change dApp Token Proxy", "QmPR9K6BevCXRVBxWGjF9RV7Pmtxr7D4gE3qsZu5bzi8GK", "MS");
+        addInitialCategories("Transfer Ether", "QmRUmxw4xmqTN6L2bSZEJfmRcU1yvVWoiMqehKtqCMAaTa", "PL");
+        addInitialCategories("Transfer Token", "QmbvmcW3zcAnng3FWgP5bHL4ba9kMMwV9G8Y8SASqrvHHB", "PL");
+        addInitialCategories("Add new version", "QmeMBNn9fs5xYVFVsN8HgupMTfgXdyz4vkLPXakWd2BY3w", "MS");
+        addInitialCategories("Add new contract", "QmWP3P58YcmveHeXqgsBCRmDewTYV1QqeQqBmRkDujrDLR", "MS");
+        addInitialCategories("Add new authorized address", "QmRczxM2yN11th3MB8159rm1qAnk4VSrYYmFQCEXXRUf9Z", "SV");
+        addInitialCategories(
+            "Upgrade a contract Implementation",
+            "Qme4hGas6RuDYk9LKE2XkK9E46LNeCBUzY12DdT5uQstvh",
+            "MS"
+        );
+        addInitialCategories(
+            "Upgrade a contract proxy",
+            "QmUNGEn7E2csB3YxohDxBKNqvzwa1WfvrSH4TCCFD9DZsg",
+            "MS"
+        );
+        addInitialCategories("Resume Proposal", "QmQPWVjmv2Gt2Dzt1rxmFkHCptFSdtX4VC5g7VVNUByLv1", "GD");
+        addInitialCategories("Pause Proposal", "QmWWoiRZCmi61LQKpGyGuKjasFVpq8JzbLPvDhU8TBS9tk", "GD");
+        addInitialCategories("Buy GBT in Pool", "QmUc6apk3aRoHPaSwafo7RkV4XTJaaWS6Q7MogTMqLDyWs", "PL");
+        addInitialCategories("Others, not specified", "", "EX");
         constructorCheck = true;
     }
 
