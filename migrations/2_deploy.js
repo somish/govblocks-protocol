f2177c2f64a6d61304af9363ef938f3e4b03090b
udkreddy <udkr1996@gmail.com>
2019-08-06 14:47:20 +0530

    removed eventcaller from migrations file

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,24 +4,14 @@ const Master = artifacts.require('Master');
 const GBTStandardToken = artifacts.require('GBTStandardToken');
 const Governance = artifacts.require('Governance');
 const ProposalCategory = artifacts.require('ProposalCategory');
-const EventCaller = artifacts.require('EventCaller');
 
 module.exports = function(deployer) {
   deployer.then(async () => {
-    ec = await deployer.deploy(EventCaller);
     gbt = await deployer.deploy(GBTStandardToken);
-    await deployer.deploy(GovBlocksMaster, ec.address);
+    await deployer.deploy(GovBlocksMaster);
     gv = await deployer.deploy(Governance);
     mr = await deployer.deploy(MemberRoles);
     pc = await deployer.deploy(ProposalCategory);
-    let implementations = [mr.address, pc.address, gv.address];
     ms = await deployer.deploy(Master);
-    await ms.initMaster(
-      mr.address,
-      true,
-      gbt.address,
-      gbt.address,
-      implementations
-    );
   });
 };
