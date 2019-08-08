c835d4b55b2abc84d2c717c38b6c121271c1bbbc
udkreddy <udkr1996@gmail.com>
2019-08-08 10:39:08 +0530

    updated governance interface

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -24,7 +24,7 @@ import "./interfaces/IGovernance.sol";
 import "./interfaces/IProposalCategory.sol";
 
 
-contract Governance is IGovernance, Upgradeable {
+contract Governance is IGovernance,Upgradeable {
 
     enum ProposalStatus { 
         Draft,
