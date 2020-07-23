3d65ed9cd8165c9337488d4d6717204b2fadda13
udkreddySomish <dushyanth@somish.com>
2020-07-23 13:05:01 +0530

    Updated ganache port

--- truffle-config.js
+++ truffle-config.js
@@ -6,14 +6,19 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 7545,
+      port: 8545,
       network_id: '5777'
     }
   },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+  compilers: {
+    solc: {
+      version: '0.4.24',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   },
   mocha: {
